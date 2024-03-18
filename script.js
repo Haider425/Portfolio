00f627ef99c1f0f313f822c2b434cbb05a477756
Haider425 <haider.saleem@ontariotechu.net>
2024-03-17 23:48:36 -0400

    almost finished the website

--- script.js
+++ script.js
@@ -5,3 +5,51 @@ function toggleMenu(){
     icon.classList.toggle('open');
 
 }
+
+// Function to toggle theme
+function toggleTheme() {
+    const root = document.documentElement;
+    const icons = document.querySelectorAll('.icons2');
+    const arrows = document.querySelectorAll('.arrow');
+    const isDarkMode = root.classList.toggle('dark');
+    
+    
+
+
+    icons.forEach(icon => {
+      if (isDarkMode) {
+        icon.style.filter = 'invert(100%) sepia(0%) saturate(13%) hue-rotate(275deg) brightness(106%) contrast(106%)';
+      } else {
+        icon.style.filter = 'none';
+      }
+    });
+  
+    // Handle arrows
+    arrows.forEach(arrow => {
+      if (isDarkMode) {
+        arrow.style.filter = 'invert(29%) sepia(62%) saturate(311%) hue-rotate(246deg) brightness(94%) contrast(94%)';
+      } else {
+        arrow.style.filter = 'invert(29%) sepia(62%) saturate(311%) hue-rotate(246deg) brightness(94%) contrast(94%)';
+      }
+    });
+  }
+
+  const toggle = document.getElementById('toggleDark');
+  const body = document.querySelector('body');
+  
+  
+  toggle.addEventListener('click', function(){
+        toggleTheme();
+      this.classList.toggle('bi-moon');
+      if(this.classList.toggle('bi-brightness-high-fill')){
+          body.style.transition = '1s';
+          toggle.style.color = 'black';
+          toggle.style.transition = '1s';
+
+      }else{
+          body.style.transition = '1s';
+            toggle.style.color = 'white';
+            toggle.style.transition = '1s';
+
+      }
+  });
